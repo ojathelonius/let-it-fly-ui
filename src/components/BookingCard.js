1050984aa050f359d4b703d3ca9519d1e36b148e
ojathelonius <louis.johanet@epfedu.fr>
2018-09-18 23:13:38 +0200

    Improve Booking card

--- src/components/BookingCard.js
+++ src/components/BookingCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import singaporeLogo from '../assets/images/logo-singapore.png';
-import { Card, Icon } from 'antd';
+import { Card, Icon, Button } from 'antd';
 import { Map, TileLayer, Marker } from 'react-leaflet'
 
 
@@ -14,10 +14,13 @@ class BookingCard extends Component {
 
         const bookingRow = {
             display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            flexWrap: 'wrap',
-            width: '100%'
+            flexDirection: 'column',
+            width: '90%',
+            backgroundColor: 'white',
+            borderRadius: '5px',
+            border: '1px solid #1f4a87',
+            position: 'relative',
+            marginBottom: '30px'
         }
 
         const tileStyle = {
@@ -37,15 +40,8 @@ class BookingCard extends Component {
             borderTop: '1px solid #1f4a87'
         }
 
-        const imgStyle = {
-            borderRadius: '2px',
-            border: '1px solid #1f4a87',
-            borderRadius: '3px',
-            margin: '5px'
-        }
-
         const mapStyle = {
-            height: '100px',
+            height: '150px',
             borderBottom: '1px solid #1f4a87'
         }
 
@@ -55,32 +51,85 @@ class BookingCard extends Component {
         };
 
         const mainBody = {
-            padding: '10px'
+            padding: '20px',
+            marginTop: '45px',
+            fontSize: '15px',
+            display: 'flex',
+            flexDirection: 'column'
+        }
+
+        const roundImgContainer = {
+            width: '150px',
+            height: '150px',
+            overflow: 'hidden',
+            borderRadius: '50%',
+            margin: '0 auto',
+            top: '90px',
+            border: '1px solid #1f4a87',
+            left: '0%',
+            right: '0%',
+            position: 'absolute',
+            zIndex: '3000'
+        }
+
+        const imgStyle = {
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            height: '100%',
+            width: 'auto',
+            transform: 'translate(-50%,-50%)'
+        }
+
+        const btnContainer = {
+            display: 'flex'
+        };
+
+        const btnStyle = {
+            flex: '1',
+            borderRadius: '0',
+            backgroundColor: '#1f4a87',
+            height: '40px',
+            border: 'none'
+        }
+
+        const iconStyle = {
+            fontSize: '18px',
+            width: '40px',
+            textAlign: 'center'
         }
 
         return (
             <div style={bookingRow}>
+                <Map center={experiencePosition} zoom={13} style={mapStyle} zoomControl={false} attributionControl={false} dragging={false} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={experiencePosition}>
+                    </Marker>
+                </Map>
+                <div style={roundImgContainer}>
+                    <img src={this.props.booking.image} alt={this.props.booking.title} style={imgStyle} />
+                </div>
+                <div style={btnContainer}>
+                    <Button type="primary" style={btnStyle}><i className="fas fa-times" style={iconStyle}></i><span className='mobile-hidden'>Cancel</span></Button>
+                    <Button type="primary" style={btnStyle}><i className="fas fa-pen" style={iconStyle}></i><span className='mobile-hidden'>Modify</span></Button>
+                </div>
+                <div style={mainBody}>
+                    <div style={{ alignSelf: 'center', marginBottom: '10px' }}>{this.props.booking.description}</div>
+                    <h2>Informations</h2>
+                    <div>
+                        <i class="fas fa-exclamation" style={iconStyle}></i>{this.props.booking.requirements ? this.props.booking.requirements : 'None'}
+                    </div>
+                    <div>
+                        <i class="far fa-clock" style={iconStyle}></i>{this.props.booking.startingAt + ' - ' + this.props.booking.endingAt}
+                    </div>
 
-                <Card
-                    title={this.props.booking.title}
-                    style={tileStyle}
-                    headStyle={headStyle}
-                    bodyStyle={bodyStyle}
-                    hoverable={true}
-                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
-                >
-                    <Map center={experiencePosition} zoom={13} style={mapStyle} zoomControl={false} attributionControl={false}>
-                        <TileLayer
-                            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Marker position={experiencePosition}>
-                        </Marker>
-                    </Map>
-                    <div style={mainBody}>{this.props.booking.description}</div>
-                    
-                </Card>
-            </div>
+                </div>
+
+
+            </div >
         );
     }
 }
