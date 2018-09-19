0619e43e9a4d898af8148fab8665d5bc8b85436f
ojathelonius <louis.johanet@epfedu.fr>
2018-09-20 00:01:17 +0200

    Improve flight layout

--- src/components/FlightCard.js
+++ src/components/FlightCard.js
@@ -7,7 +7,7 @@ class FlightCard extends Component {
         const tileStyle = {
             margin: '20px',
             border: '1px solid #fcb130',
-            width: '500px'
+            flex: '0 1 500px'
         }
 
         const bodyStyle = {
@@ -20,22 +20,39 @@ class FlightCard extends Component {
 
         const rowItinerary = {
             display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center'
+            justifyContent: 'center',
+            alignItems: 'center',
+            boxShadow: 'inset 0 0 4px #000000',
+            margin: '10px',
+            width: '95%',
+            flexWrap: 'wrap'
         }
 
         const currentWeather = this.props.isFetchingWeather ? 'Fetching data...' :
             (<p>It's currently {this.props.weather.temperature} degrees in {this.props.weather.city}, although it feels more like {this.props.weather.feelsLikeTemperature}.</p>)
 
+        const itinerary = [].concat(...this.props.flight.itinerary.map(e => ['arrow', e])).slice(1);
+
+        const iconStyle = {
+            fontSize: '18px',
+            width: '40px',
+            textAlign: 'center'
+        }
+
+        const iconContainer = {
+            display: 'flex',
+            alignItems: 'center'
+        }
 
         return (
             <div style={rowItinerary}>
-                {this.props.flight.itinerary.map((curFlight, index, arr) => {
-                    const arrow = (arr.length -1 === index) ?
-                    '' 
-                    : (<i class="fas fa-arrow-right"></i>);
+                {itinerary.map((curFlight, index, arr) => {
 
+                    if (curFlight == 'arrow') {
+                        return (<i className="fas fa-arrow-right mobile-hidden" style={{ color: 'white' }}></i>);
+                    }
                     return (
+
                         <Card
                             title={<span>{curFlight.code} - {curFlight.fromCity} to {curFlight.toCity}</span>}
                             style={tileStyle}
@@ -43,6 +60,18 @@ class FlightCard extends Component {
                             bodyStyle={bodyStyle}
                             hoverable={true}
                         >
+
+
+                            <div style={iconContainer}>
+                                <i class="far fa-calendar" style={iconStyle}></i>{curFlight.departureDate}
+                            </div>
+                            <div style={iconContainer}> 
+                                <i class="far fa-clock" style={iconStyle}></i>{curFlight.departureTime} ({curFlight.duration})
+                            </div>
+                            <div style={iconContainer}>
+                                <i class="fas fa-plane" style={iconStyle}></i>{curFlight.airline}
+                            </div>
+
                         </Card>
 
                     )
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    apiUrl : 'http://localhost:3001/api'
+    apiUrl : 'https://api.johanet.fr/api'
 }
